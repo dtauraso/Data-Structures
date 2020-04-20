f0a5dc995d4e30ffc7f8dc9293a2df8c65d35a2b
David Tauraso <david318@me.com>
2020-04-20 12:00:34 -0700

    Did stack

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,9 +7,12 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Modifying the order of elements is O(1) time, because they are not stored strictly in a sequence .
+
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
+        
         pass
 
     def dequeue(self):
